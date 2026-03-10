573bc04c02a71868fb8719aa7d8c26433e7ce9e7
joshua <jnyakundi31@yahoo.com>
2026-03-10 16:46:22 +0300

    gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import '@/app/ui/global.css';
+import styles from  '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css';
+import { styleText } from 'util';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -28,6 +31,9 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
         </div>
+        <div
+          className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+        />
       </div>
     </main>
   );
