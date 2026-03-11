36bdc039ea626cf65adde7aa113bee75c3276cf9
joshua <jnyakundi31@yahoo.com>
2026-03-11 15:36:23 +0300

    navigating between pages using the Link

--- .next/dev/server/app/page.js
+++ .next/dev/server/app/page.js
@@ -1,6 +1,6 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
 R.c("server/chunks/ssr/node_modules__pnpm_f87fa3a6._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__ed4e87b5._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__6c69cc18._.js")
 R.c("server/chunks/ssr/1dff6_next_dist_e861362a._.js")
 R.c("server/chunks/ssr/[root-of-the-server]__7f148858._.js")
 R.c("server/chunks/ssr/1dff6_next_dist_client_components_builtin_global-error_66953fd8.js")
